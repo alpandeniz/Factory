4ba95f87d62f1e05efa9073db9bea2bced237d26
Alpan <alpandnz48@gmail.com>
2022-12-03 13:25:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee worker1 = new Employee("Ahmet Çalışkan",1001,41,2021);
+		Employee worker2 = new Employee("Alpan Deniz", 1000, 1,2021);
+		
+		
+		worker1.toString();
+		
+		
+		
+	}
+
+}
